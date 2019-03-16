417bfb8f520ea4e5145d2c11e3cbe4018b3a3a0a
Akihisa Shimada <empereur-7@outlook.jp>
2019-03-17 01:42:16 +0900

    add Filter

--- src/input/ParamListServlet.java
+++ src/input/ParamListServlet.java
@@ -30,7 +30,8 @@ public class ParamListServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		request.setCharacterEncoding("UTF-8");
+		// EncodingFileterで実施しているためコメントアウト
+//		request.setCharacterEncoding("UTF-8");
 
 		response.setContentType("text/plain; charset=UTF-8");
 		PrintWriter out = response.getWriter();
